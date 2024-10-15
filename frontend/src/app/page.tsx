a3be0b4cdeb6106a22e1e34a52f119aeb0f2e489
arcadetokyo <pepperpiccio@gmail.com>
2024-10-16 01:32:08 +0800

    home and help page revamp

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,30 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import localFont from 'next/font/local';
+
+const gauPopMagic = localFont({
+  src: './GAU_pop_magic.woff2',
+  display: 'swap',
+});
 
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen min-w-fit bg-gray-50">
-      <h1 className="text-3xl font-bold mb-8">Keiko</h1>
+      <h1 className={`text-7xl font-bold mb-8 ${gauPopMagic.className} text-red-500`}>
+        Keiko!
+      </h1>
       <div className="flex flex-col items-center space-y-4 w-50 max-w-xs">
         <Link href="/dashboard" className="w-full">
-          <Button className="w-full" asChild>
-            <p>Start</p>
+          <Button className="w-full bg-white text-red-500 hover:bg-red-500 hover:text-white border border-red-500" asChild>
+            <p className={`${gauPopMagic.className}`}>⟡ Start</p> 
           </Button>
         </Link>
+        
         <Link href="/help" className="w-full">
-          <Button className="w-full" variant="outline" asChild>
-            <p>How it works</p>
+          <Button className="w-full bg-red-500 text-white hover:bg-white hover:text-red-500 border border-red-500" asChild>
+            <p className={`${gauPopMagic.className}`}>✦ How it works</p> 
           </Button>
         </Link>
       </div>
