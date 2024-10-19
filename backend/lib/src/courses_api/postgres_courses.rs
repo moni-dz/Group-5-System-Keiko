ce4acf24d650b6df9cc75cf9317d67c92854ed21
JohnYncierto <163652271+JohnYncierto@users.noreply.github.com>
2024-10-19 12:24:47 +0800

    Update postgres_courses.rs

--- backend/lib/src/courses_api/postgres_courses.rs
+++ backend/lib/src/courses_api/postgres_courses.rs
@@ -7,26 +7,67 @@ use uuid::Uuid;
 impl CoursesAPI for KeikoDatabase {
     // GET /v1/courses
     async fn get_courses(&self) -> CourseResult<Vec<Course>> {
-        todo!()
+        sqlx::query_as::<_, Course>("SELECT * FROM courses")
+        .fetch_all(&self.pool)
+        .await
+        .map_err(|e| e.to_string())
     }
 
     // GET /v1/courses/{course_id}
     async fn get_course(&self, course_id: &Uuid) -> CourseResult<Course> {
-        todo!()
+        sqlx::query_as::<_, Course>("SELECT * FROM courses WHERE id = $1")
+        .bind(course_id)
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| e.to_string())
     }
 
     // POST /v1/courses
     async fn create_course(&self, create_course: &CreateCourse) -> CourseResult<Course> {
-        todo!()
+        sqlx::query_as::<_, Course>(
+            r#"
+            INSERT INTO courses (name, description)
+            VALUES ($1, $2)
+            RETURNING id, name, description, created_at, updated_at
+            "#,
+        )
+        .bind(&create_course.name)
+        .bind(&create_course.description)
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| e.to_string())
     }
 
     // PUT /v1/courses
     async fn update_course(&self, course: &Course) -> CourseResult<Course> {
-        todo!()
+        sqlx::query_as::<_, Course>(
+            r#"
+            UPDATE courses
+            SET name = $2, description = $3, updated_at = now()
+            WHERE id = $1
+            RETURNING id, name, description, created_at, updated_at
+            "#,
+        )
+        .bind(course.id)
+        .bind(&course.name)
+        .bind(&course.description)
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| e.to_string())
     }
 
     // DELETE /v1/courses/{course_id}
     async fn delete_course(&self, course_id: &Uuid) -> CourseResult<Uuid> {
-        todo!()
+        sqlx::query_scalar::<_, Uuid>(
+            r#"
+            DELETE FROM courses
+            WHERE id = $1
+            RETURNING id
+            "#,
+        )
+        .bind(course_id)
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| e.to_string())
     }
 }
