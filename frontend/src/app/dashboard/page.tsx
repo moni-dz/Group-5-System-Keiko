7f23efaf33436ee386341f5e8e4570f503242ea1
moni <lythe1107@gmail.com>
2024-10-22 00:01:35 +0800

    add suspense tag

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ import {
 import dayjs from "dayjs";
 import Image from "next/image";
 import logo from "../../public/logo.png";
-import { useState, useEffect } from "react";
+import { useState, Suspense } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -31,6 +31,7 @@ import { redirect, useSearchParams, useRouter } from "next/navigation";
 import Link from "next/link";
 import { CourseData, getAllCourses } from "@/lib/api";
 import { useToast } from "@/hooks/use-toast";
+import DashboardLoading from "./loading";
 
 export default function MainPage() {
   const router = useRouter();
@@ -47,11 +48,7 @@ export default function MainPage() {
   });
 
   if (isPending) {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-background/80 backdrop-blur-sm">
-        <Loader2 className="h-8 w-8 animate-spin text-primary" />
-      </div>
-    );
+    return <DashboardLoading />;
   }
 
   if (isError) {
@@ -139,183 +136,185 @@ export default function MainPage() {
   };
 
   return (
-    <div className={`flex h-screen bg-gray-100 text-extrabold`}>
-      {/* Sidebar */}
-      <aside
-        className={`transition-all duration-300 ${isSidebarCollapsed ? "w-16 bg-white text-zinc-500" : "w-64 bg-white shadow-md"}`}
-      >
-        <div className="p-4 flex flex-col h-full">
-          <div className="flex-1">
-            <div className="flex justify-center items-center mb-6">
-              {isSidebarCollapsed ? (
-                <Image src={logo} alt="logo" />
-              ) : (
-                <h1 className={`text-3xl font-bold font-gau-pop-magic text-red-500 text-center`}>Keiko!</h1>
-              )}
+    <Suspense fallback={<DashboardLoading />}>
+      <div className={`flex h-screen bg-gray-100 text-extrabold`}>
+        {/* Sidebar */}
+        <aside
+          className={`transition-all duration-300 ${isSidebarCollapsed ? "w-16 bg-white text-zinc-500" : "w-64 bg-white shadow-md"}`}
+        >
+          <div className="p-4 flex flex-col h-full">
+            <div className="flex-1">
+              <div className="flex justify-center items-center mb-6">
+                {isSidebarCollapsed ? (
+                  <Image src={logo} alt="logo" />
+                ) : (
+                  <h1 className={`text-3xl font-bold font-gau-pop-magic text-red-500 text-center`}>Keiko!</h1>
+                )}
+              </div>
+
+              <nav className="flex flex-col">
+                <Link href="?view=courses">
+                  <Button
+                    variant="ghost"
+                    className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
+                  >
+                    {isSidebarCollapsed ? (
+                      <ClipboardList />
+                    ) : (
+                      <>
+                        <ClipboardList className="mr-2" />
+                        Courses
+                      </>
+                    )}
+                  </Button>
+                </Link>
+
+                <Link href="?view=ongoing">
+                  <Button
+                    variant="ghost"
+                    className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
+                  >
+                    {isSidebarCollapsed ? (
+                      <Clock />
+                    ) : (
+                      <>
+                        <Clock className="mr-2" />
+                        On-Going
+                      </>
+                    )}
+                  </Button>
+                </Link>
+
+                <Link href="?view=completed">
+                  <Button
+                    variant="ghost"
+                    className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
+                  >
+                    {isSidebarCollapsed ? (
+                      <BookCheck />
+                    ) : (
+                      <>
+                        <BookCheck className="mr-2" />
+                        Completed
+                      </>
+                    )}
+                  </Button>
+                </Link>
+              </nav>
             </div>
 
-            <nav className="flex flex-col">
-              <Link href="?view=courses">
-                <Button
-                  variant="ghost"
-                  className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
-                >
-                  {isSidebarCollapsed ? (
-                    <ClipboardList />
-                  ) : (
-                    <>
-                      <ClipboardList className="mr-2" />
-                      Courses
-                    </>
-                  )}
-                </Button>
-              </Link>
-
-              <Link href="?view=ongoing">
-                <Button
-                  variant="ghost"
-                  className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
-                >
-                  {isSidebarCollapsed ? (
-                    <Clock />
-                  ) : (
-                    <>
-                      <Clock className="mr-2" />
-                      On-Going
-                    </>
-                  )}
-                </Button>
-              </Link>
-
-              <Link href="?view=completed">
-                <Button
-                  variant="ghost"
-                  className={`w-full flex items-center mb-2 text-1.5xl text-zinc-500 ${isSidebarCollapsed ? `px-2 font-gau-pop-magic` : ""} justify-start hover:bg-red-500 active:bg-red-500 hover:text-white active:text-white`}
-                >
-                  {isSidebarCollapsed ? (
-                    <BookCheck />
-                  ) : (
-                    <>
-                      <BookCheck className="mr-2" />
-                      Completed
-                    </>
-                  )}
-                </Button>
-              </Link>
-            </nav>
+            <div className="flex justify-center items-center mt-auto">
+              <Button variant="ghost" size="icon" onClick={toggleSidebar}>
+                {isSidebarCollapsed ? (
+                  <Menu className="text-zinc-500" />
+                ) : (
+                  <div className="flex items-center">
+                    <ChevronsLeft className="text-red-500" />
+                  </div>
+                )}
+              </Button>
+            </div>
           </div>
+        </aside>
 
-          <div className="flex justify-center items-center mt-auto">
-            <Button variant="ghost" size="icon" onClick={toggleSidebar}>
-              {isSidebarCollapsed ? (
-                <Menu className="text-zinc-500" />
-              ) : (
-                <div className="flex items-center">
-                  <ChevronsLeft className="text-red-500" />
-                </div>
-              )}
+        {/* Main content */}
+        <main className="flex-1 p-8">
+          <div className="flex justify-between items-center mb-8">
+            <div className="relative w-1/2">
+              <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-red-400 text-semibold font-sans" />
+              <Input
+                type="text"
+                placeholder="search for courses..."
+                className="pl-10 pr-4 py-2 w-full"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </div>
+
+            <Button variant="ghost" className="p-2" onClick={() => router.push("/")}>
+              <Home className="h-5 w-5 text-red-500 bg-gray-100 hover:text-white hover:bg-red-500 rounded-sm" />
             </Button>
           </div>
-        </div>
-      </aside>
 
-      {/* Main content */}
-      <main className="flex-1 p-8">
-        <div className="flex justify-between items-center mb-8">
-          <div className="relative w-1/2">
-            <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-red-400 text-semibold font-sans" />
-            <Input
-              type="text"
-              placeholder="search for courses..."
-              className="pl-10 pr-4 py-2 w-full"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-          </div>
-
-          <Button variant="ghost" className="p-2" onClick={() => router.push("/")}>
-            <Home className="h-5 w-5 text-red-500 bg-gray-100 hover:text-white hover:bg-red-500 rounded-sm" />
-          </Button>
-        </div>
-
-        {activeView === "courses" ? (
-          <div>
-            <h2 className={`text-2xl font-bold mb-4 font-gau-pop-magic text-red-500`}>COURSES</h2>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCourses)}</div>
-              <div className="bg-white shadow-md rounded-lg p-4">
-                {renderCourseDetails(selectedCourse, filteredCourses)}
+          {activeView === "courses" ? (
+            <div>
+              <h2 className={`text-2xl font-bold mb-4 font-gau-pop-magic text-red-500`}>COURSES</h2>
+              <div className="grid grid-cols-2 gap-4">
+                <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCourses)}</div>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                  {renderCourseDetails(selectedCourse, filteredCourses)}
+                </div>
               </div>
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div className="space-x-2">
-                <Button
-                  className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white"
-                  onClick={() => router.push("/courses")}
-                >
-                  <Plus className="mr-2 h-4 w-4 hover:text-white" />
-                  Add
-                </Button>
+              <div className="mt-4 flex justify-between">
+                <div className="space-x-2">
+                  <Button
+                    className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white"
+                    onClick={() => router.push("/courses")}
+                  >
+                    <Plus className="mr-2 h-4 w-4 hover:text-white" />
+                    Add
+                  </Button>
+                  <Button className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white">
+                    <CircleX className="mr-2 h-4 w-4" />
+                    Delete
+                  </Button>
+                </div>
                 <Button className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white">
-                  <CircleX className="mr-2 h-4 w-4" />
-                  Delete
+                  <Edit className="mr-2 h-4 w-4" />
+                  Edit
                 </Button>
               </div>
-              <Button className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white">
-                <Edit className="mr-2 h-4 w-4" />
-                Edit
-              </Button>
             </div>
-          </div>
-        ) : activeView === "ongoing" ? (
-          <div className="space-y-4">
-            <div>
-              <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">ON-GOING COURSES</h2>
-              <div className="grid grid-cols-2 gap-4">
-                <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredOngoingCourses)}</div>
-                <div className="bg-white shadow-md rounded-lg p-4">
-                  {renderCourseDetails(selectedCourse, filteredOngoingCourses)}
+          ) : activeView === "ongoing" ? (
+            <div className="space-y-4">
+              <div>
+                <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">ON-GOING COURSES</h2>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredOngoingCourses)}</div>
+                  <div className="bg-white shadow-md rounded-lg p-4">
+                    {renderCourseDetails(selectedCourse, filteredOngoingCourses)}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mt-4">
-              <Button
-                className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2"
-                onClick={() => redirect(`/quiz/${selectedCourse}`)}
-              >
-                <Pen width="20" height="20" />
-                <span>Start Quiz</span>
-              </Button>
-            </div>
-          </div>
-        ) : activeView === "completed" ? (
-          <div className="space-y-4">
-            <div>
-              <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">COMPLETED COURSES</h2>
-              <div className="grid grid-cols-2 gap-4">
-                <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCompletedCourses)}</div>
-                <div className="bg-white shadow-md rounded-lg p-4">
-                  {renderCourseDetails(selectedCourse, filteredCompletedCourses)}
-                </div>
+              <div className="mt-4">
+                <Button
+                  className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2"
+                  onClick={() => redirect(`/quiz/${selectedCourse}`)}
+                >
+                  <Pen width="20" height="20" />
+                  <span>Start Quiz</span>
+                </Button>
               </div>
             </div>
-            <div className="mt-4 flex space-x-4">
-              <Button className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2">
-                <FolderClock width="20" height="20" />
-                <span>Mark as Ongoing</span>
-              </Button>
-              <Button className="bg-white text-red-500 border border-red-500 hover:border-zinc-500 hover:bg-zinc-500 hover:text-white flex items-center space-x-2">
-                <FileChartLine width="24" height="24" />
-                <span>Course Analytics</span>
-              </Button>
+          ) : activeView === "completed" ? (
+            <div className="space-y-4">
+              <div>
+                <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">COMPLETED COURSES</h2>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCompletedCourses)}</div>
+                  <div className="bg-white shadow-md rounded-lg p-4">
+                    {renderCourseDetails(selectedCourse, filteredCompletedCourses)}
+                  </div>
+                </div>
+              </div>
+              <div className="mt-4 flex space-x-4">
+                <Button className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2">
+                  <FolderClock width="20" height="20" />
+                  <span>Mark as Ongoing</span>
+                </Button>
+                <Button className="bg-white text-red-500 border border-red-500 hover:border-zinc-500 hover:bg-zinc-500 hover:text-white flex items-center space-x-2">
+                  <FileChartLine width="24" height="24" />
+                  <span>Course Analytics</span>
+                </Button>
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="bg-white shadow-md rounded-lg p-8">
-            <h2 className={`text-3xl font-bold text-zinc-300 font-gau-pop-magic`}>Get Started</h2>
-          </div>
-        )}
-      </main>
-    </div>
+          ) : (
+            <div className="bg-white shadow-md rounded-lg p-8">
+              <h2 className={`text-3xl font-bold text-zinc-300 font-gau-pop-magic`}>Get Started</h2>
+            </div>
+          )}
+        </main>
+      </div>
+    </Suspense>
   );
 }
