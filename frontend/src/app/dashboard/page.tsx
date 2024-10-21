431629f7aaee0bbbeebc480a2f13ff69fd0c5a53
moni <lythe1107@gmail.com>
2024-10-22 00:32:09 +0800

    separate dashboard components into its own file

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -17,24 +17,21 @@ import {
   CircleX,
   FolderClock,
   FileChartLine,
-  Loader2,
 } from "lucide-react";
-import dayjs from "dayjs";
 import Image from "next/image";
 import logo from "../../public/logo.png";
 import { useState, Suspense } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
-import { redirect, useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { CourseData, getAllCourses } from "@/lib/api";
 import { useToast } from "@/hooks/use-toast";
 import DashboardLoading from "./loading";
+import { CourseDetails, CourseList } from "./components";
 
 export default function MainPage() {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const { toast } = useToast();
   const activeView = searchParams.get("view") || "";
@@ -77,63 +74,6 @@ export default function MainPage() {
   const filteredCompletedCourses = filterCourses(completedCourses, searchQuery);
 
   const toggleSidebar = () => setIsSidebarCollapsed(!isSidebarCollapsed);
-
-  const renderCourseList = (courses: CourseData[]) => (
-    <ul>
-      {courses.map((course) => (
-        <Link href={`?view=${activeView}&course=${course.course_code}`} key={course.id}>
-          <li
-            className={`mb-2 p-2 rounded cursor-pointer ${
-              selectedCourse === course.course_code ? "bg-red-100" : "hover:bg-red-100"
-            } font-semibold text-zinc-500`}
-          >
-            {course.course_code}
-          </li>
-        </Link>
-      ))}
-    </ul>
-  );
-
-  const renderCourseDetails = (course_code: string, courses: CourseData[]) => {
-    let course = courses.find((course) => course.course_code === course_code);
-
-    return course === undefined ? (
-      <p className="text-zinc-500 italic">✦ select a course to view details...</p>
-    ) : (
-      <div>
-        <h3 className="text-xl italic font-semibold text-red-500 font-gau-pop-magic mb-2">
-          {course.name || "Course Name"}
-        </h3>
-
-        <p className="text-zinc-500 font-semibold">
-          <span className="font-bold">Course Code:</span> {course.course_code || "N/A"}
-        </p>
-
-        <p className="text-zinc-500 font-semibold">
-          <span className="font-bold">Description:</span> {course.description || "No description available."}
-        </p>
-
-        <p className="text-zinc-500 font-semibold">
-          <span className="font-bold">Number of Questions:</span>{" "}
-          {course.questions != 0 ? `${course.questions} questions` : "N/A"}
-        </p>
-
-        {course.progress !== undefined && (
-          <div className="mt-2">
-            <p className="text-zinc-500">Progress: {course.progress}%</p>
-            <div className="w-full bg-gray-200 rounded-full h-2.5">
-              <Progress value={course.progress} indicatorColor="bg-red-500" className="bg-gray-200 h-2.5" />
-            </div>
-          </div>
-        )}
-        {course.is_completed && (
-          <p className="text-zinc-500 italic mt-2">
-            Completed on: {dayjs(course.completion_date).format("MM-DD-YYYY")}
-          </p>
-        )}
-      </div>
-    );
-  };
 
   return (
     <Suspense fallback={<DashboardLoading />}>
@@ -231,29 +171,32 @@ export default function MainPage() {
               />
             </div>
 
-            <Button variant="ghost" className="p-2" onClick={() => router.push("/")}>
-              <Home className="h-5 w-5 text-red-500 bg-gray-100 hover:text-white hover:bg-red-500 rounded-sm" />
-            </Button>
+            <Link href="/">
+              <Button variant="ghost" className="p-2">
+                <Home className="h-5 w-5 text-red-500 bg-gray-100 hover:text-white hover:bg-red-500 rounded-sm" />
+              </Button>
+            </Link>
           </div>
 
           {activeView === "courses" ? (
             <div>
               <h2 className={`text-2xl font-bold mb-4 font-gau-pop-magic text-red-500`}>COURSES</h2>
               <div className="grid grid-cols-2 gap-4">
-                <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCourses)}</div>
                 <div className="bg-white shadow-md rounded-lg p-4">
-                  {renderCourseDetails(selectedCourse, filteredCourses)}
+                  <CourseList courses={filteredCourses} activeView={activeView} course_code={selectedCourse} />
+                </div>
+                <div className="bg-white shadow-md rounded-lg p-4">
+                  <CourseDetails courses={filteredCourses} course_code={selectedCourse} />
                 </div>
               </div>
               <div className="mt-4 flex justify-between">
                 <div className="space-x-2">
-                  <Button
-                    className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white"
-                    onClick={() => router.push("/courses")}
-                  >
-                    <Plus className="mr-2 h-4 w-4 hover:text-white" />
-                    Add
-                  </Button>
+                  <Link href="/courses">
+                    <Button className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white">
+                      <Plus className="mr-2 h-4 w-4 hover:text-white" />
+                      Add
+                    </Button>
+                  </Link>
                   <Button className="bg-white text-red-500 border border-red-500 hover:border-red-500 hover:bg-red-500 hover:text-white">
                     <CircleX className="mr-2 h-4 w-4" />
                     Delete
@@ -270,20 +213,21 @@ export default function MainPage() {
               <div>
                 <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">ON-GOING COURSES</h2>
                 <div className="grid grid-cols-2 gap-4">
-                  <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredOngoingCourses)}</div>
                   <div className="bg-white shadow-md rounded-lg p-4">
-                    {renderCourseDetails(selectedCourse, filteredOngoingCourses)}
+                    <CourseList courses={filteredOngoingCourses} activeView={activeView} course_code={selectedCourse} />
+                  </div>
+                  <div className="bg-white shadow-md rounded-lg p-4">
+                    <CourseDetails courses={filteredOngoingCourses} course_code={selectedCourse} />
                   </div>
                 </div>
               </div>
               <div className="mt-4">
-                <Button
-                  className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2"
-                  onClick={() => redirect(`/quiz/${selectedCourse}`)}
-                >
-                  <Pen width="20" height="20" />
-                  <span>Start Quiz</span>
-                </Button>
+                <Link href={`/quiz/${selectedCourse}`}>
+                  <Button className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2">
+                    <Pen width="20" height="20" />
+                    <span>Start Quiz</span>
+                  </Button>
+                </Link>
               </div>
             </div>
           ) : activeView === "completed" ? (
@@ -291,9 +235,15 @@ export default function MainPage() {
               <div>
                 <h2 className="text-xl font-bold font-gau-pop-magic text-red-500 mb-4">COMPLETED COURSES</h2>
                 <div className="grid grid-cols-2 gap-4">
-                  <div className="bg-white shadow-md rounded-lg p-4">{renderCourseList(filteredCompletedCourses)}</div>
                   <div className="bg-white shadow-md rounded-lg p-4">
-                    {renderCourseDetails(selectedCourse, filteredCompletedCourses)}
+                    <CourseList
+                      courses={filteredCompletedCourses}
+                      activeView={activeView}
+                      course_code={selectedCourse}
+                    />
+                  </div>
+                  <div className="bg-white shadow-md rounded-lg p-4">
+                    <CourseDetails courses={filteredCompletedCourses} course_code={selectedCourse} />
                   </div>
                 </div>
               </div>
