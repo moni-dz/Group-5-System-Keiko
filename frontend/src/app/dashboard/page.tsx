820129e193d5a76948360b2b15d3eb90a2dc4467
moni <lythe1107@gmail.com>
2024-10-23 04:26:30 +0800

    reorganize code

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -39,6 +39,7 @@ export default function MainPage() {
   const selectedCourse = searchParams.get("course") || "";
   const [searchQuery, setSearchQuery] = useState("");
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const toggleSidebar = () => setIsSidebarCollapsed(!isSidebarCollapsed);
 
   const { data, error, isPending, isError } = useQuery({
     queryKey: ["courses"],
@@ -70,22 +71,33 @@ export default function MainPage() {
   }
 
   const courses = data;
-  const ongoingCourses = courses.filter((course: CourseData) => !course.is_completed);
-  const completedCourses = courses.filter((course: CourseData) => course.is_completed);
 
-  const filterCourses = (courses: CourseData[], query: string) => {
+  function filterCourses(courses: CourseData[], query: string) {
     return courses.filter(
       (course) =>
         course.course_code.toLowerCase().includes(query.toLowerCase()) ||
         course.name.toLowerCase().includes(query.toLowerCase()),
     );
-  };
+  }
 
+  const ongoingCourses = courses.filter((course: CourseData) => !course.is_completed);
+  const completedCourses = courses.filter((course: CourseData) => course.is_completed);
   const filteredCourses = filterCourses(courses, searchQuery);
   const filteredOngoingCourses = filterCourses(ongoingCourses, searchQuery);
   const filteredCompletedCourses = filterCourses(completedCourses, searchQuery);
 
-  const toggleSidebar = () => setIsSidebarCollapsed(!isSidebarCollapsed);
+  function markCourseAsCompleted() {
+    const course = filteredCompletedCourses.find((course) => course.course_code === selectedCourse);
+
+    if (course) {
+      markCompletionMutation({ course_id: course.id, is_completed: false });
+
+      filteredCompletedCourses.splice(
+        filteredCompletedCourses.findIndex((course: CourseData): boolean => course.course_code === selectedCourse),
+        1,
+      );
+    }
+  }
 
   return (
     <Suspense fallback={<Loading />}>
@@ -260,35 +272,21 @@ export default function MainPage() {
                 </div>
               </div>
               <div className="mt-4 flex space-x-4">
-  <Button
-    className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2"
-    onClick={() => {
-      const course = filteredCompletedCourses.find((course) => course.course_code === selectedCourse);
+                <Button
+                  className="bg-red-500 text-white hover:bg-zinc-500 flex items-center space-x-2"
+                  onClick={() => markCourseAsCompleted()}
+                >
+                  <FolderClock width="20" height="20" />
+                  <span>Mark as Ongoing</span>
+                </Button>
 
-      if (course) {
-        markCompletionMutation({ course_id: course.id, is_completed: false });
-
-        filteredCompletedCourses.splice(
-          filteredCompletedCourses.findIndex(
-            (course: CourseData): boolean => course.course_code === selectedCourse,
-          ),
-          1,
-        );
-      }
-    }}
-  >
-    <FolderClock width="20" height="20" />
-    <span>Mark as Ongoing</span>
-  </Button>
-  
-  <Link href="/analytics">
-    <Button className="bg-white text-red-500 border border-red-500 hover:border-zinc-500 hover:bg-zinc-500 hover:text-white flex items-center space-x-2">
-      <FileChartLine width="24" height="24" />
-      <span>Course Analytics</span>
-    </Button>
-  </Link>
-</div>
-
+                <Link href={`/analytics/${selectedCourse}`}>
+                  <Button className="bg-white text-red-500 border border-red-500 hover:border-zinc-500 hover:bg-zinc-500 hover:text-white flex items-center space-x-2">
+                    <FileChartLine width="24" height="24" />
+                    <span>Course Analytics</span>
+                  </Button>
+                </Link>
+              </div>
             </div>
           ) : (
             <div className="bg-white shadow-md rounded-lg p-8">
