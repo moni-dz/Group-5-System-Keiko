89ff8546ce1e5b1258f668ff7053def70df9beab
arcadetokyo <pepperpiccio@gmail.com>
2024-10-31 14:45:10 +0800

    edited layout

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -35,7 +35,7 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <h3
     ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", className)}
+    className={cn("font-semibold leading-none tracking-tight text-zinc-500", className)}
     {...props}
   />
 ))
@@ -57,7 +57,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-6 pt-0 text-zinc-500 font-semibold", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
