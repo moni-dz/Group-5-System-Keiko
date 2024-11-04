17e65b3d94aaef66c5938139ffce0472478fd1ba
moni <lythe1107@gmail.com>
2024-11-05 00:44:31 +0800

    aggressive prefetching

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,7 +1,35 @@
 "use client";
 
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { isServer, QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -9,7 +37,11 @@ interface ProvidersProps {
 
 export default function Providers(props: ProvidersProps) {
   const { children } = props;
-  const queryClient = new QueryClient();
+  const queryClient = getQueryClient();
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NuqsAdapter>{children}</NuqsAdapter>
+    </QueryClientProvider>
+  );
 }
--- frontend/src/app/review/[quiz_id]/page.tsx
+++ frontend/src/app/review/[quiz_id]/page.tsx
@@ -1,109 +1,29 @@
-"use client";
-
-import { use, useState } from "react";
-import { SkeletonCard } from "@/components/cards";
-import { Button } from "@/components/ui/button";
-import { getCardsByQuizId, getQuiz } from "@/lib/api";
-import dynamic from "next/dynamic";
-import { Link } from "next-view-transitions";
-import { Home, ArrowLeft, ArrowRight } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
-import { ErrorSkeleton } from "@/components/status";
-
-const Flashcard = dynamic(() => import("@/components/cards").then((mod) => mod.Flashcard), {
-  loading: () => <SkeletonCard />,
-});
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { getQuiz, getCardsByQuizId } from "@/lib/api";
+import Review from "./../review";
 
 interface ReviewPageProps {
   params: Promise<{ quiz_id: string }>;
 }
 
-export default function ReviewPage({ params }: ReviewPageProps) {
-  const { quiz_id } = use(params);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+export default async function ReviewPage(props: ReviewPageProps) {
+  const { quiz_id } = await props.params;
+  const queryClient = new QueryClient();
 
-  const {
-    data: quiz,
-    isFetching: isQuizFetching,
-    isError: isQuizError,
-    error: quizError,
-  } = useQuery({
+  await queryClient.prefetchQuery({
     queryKey: ["quiz", quiz_id],
     queryFn: () => getQuiz(quiz_id),
   });
 
-  const {
-    data: cards,
-    isFetching: isCardsFetching,
-    isError: isCardsError,
-    error: cardsError,
-  } = useQuery({
+  await queryClient.prefetchQuery({
     queryKey: ["cards", quiz_id],
     queryFn: () => getCardsByQuizId(quiz_id),
     initialData: [],
   });
 
-  const handlePrevCard = () => {
-    if (currentCardIndex > 0) setCurrentCardIndex(currentCardIndex - 1);
-  };
-
-  const handleNextCard = () => {
-    if (currentCardIndex < cards.length - 1) setCurrentCardIndex(currentCardIndex + 1);
-  };
-
-  const handleExit = () => {
-    // Logic for handling exit
-  };
-
-  if (isQuizFetching || isCardsFetching) return <SkeletonCard />;
-
-  if (isQuizError || isCardsError)
-    return <ErrorSkeleton error={isQuizError ? quizError : isCardsError ? cardsError : undefined} />;
-
   return (
-    <div className="bg-gray-50 min-h-screen relative">
-      <div className="flex justify-between items-start p-4">
-        <div className="flex items-center">
-          <span className="text-red-500 font-gau-pop-magic text-2xl font-bold">REVIEW:</span>
-          <span className="text-zinc-500 ml-2 font-gau-pop-magic text-2xl">
-            {quiz?.course_code} - {quiz?.category}
-          </span>
-        </div>
-        <div className="">
-          <Link href="/dashboard?view=courses">
-          <Button onClick={handleExit} variant="outline" className="p-4">
-            <span className="text-red-500">Exit</span>
-          </Button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center justify-center mb-5">
-        <h1 className="text-3xl mb-2 mt-10 font-gau-pop-magic text-red-500 font-bold"></h1>
-      </div>
-
-      <div className="space-y-4 mt-4">
-        <div className="grid grid-rows-2 gap-5">
-          <Link href="/dashboard">
-            <Button asChild>Go Back</Button>
-          </Link>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center">
-        <div className="flex flex-wrap justify-center gap-4">
-          {isCardsFetching ? <SkeletonCard /> : cards.length > 0 && <Flashcard {...cards[currentCardIndex]} />}
-        </div>
-      </div>
-
-      <div className="flex justify-center mt-4 space-x-4">
-        <Button onClick={handlePrevCard} disabled={currentCardIndex === 0} variant="ghost">
-          <ArrowLeft className="w-6 h-6 text-zinc-500 hover:text-red-500" />
-        </Button>
-        <Button onClick={handleNextCard} disabled={currentCardIndex === cards.length - 1} variant="ghost">
-          <ArrowRight className="w-6 h-6 text-zinc-500 hover:text-red-500" />
-        </Button>
-      </div>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Review quiz_id={quiz_id} />
+    </HydrationBoundary>
   );
 }
