7aa3fb03e2cec68d0dfc98395fe7c375fcfc0122
arcadetokyo <pepperpiccio@gmail.com>
2024-11-07 18:01:53 +0800

    polish

--- frontend/src/app/dashboard/dashboard.tsx
+++ frontend/src/app/dashboard/dashboard.tsx
@@ -103,7 +103,7 @@ export default function Dashboard() {
               <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-red-400 font-semibold font-sans" />
               <Input
                 type="text"
-                placeholder={activeView === "courses" ? "search for courses..." : "search for quizzes..."}
+                placeholder={activeView === "courses" ? "✦ search for a course..." : "✦ search for a quiz..."}
                 className="pl-10 pr-4 py-2 w-full"
                 value={searchTerm ?? ""}
                 onChange={(e) => setSearchTerm(e.target.value)}
