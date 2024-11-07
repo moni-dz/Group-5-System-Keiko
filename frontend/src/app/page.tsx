6eef0f5b1ae6fb51b3d755493be4a3480b1bf815
moni <lythe1107@gmail.com>
2024-11-08 02:29:14 +0800

    prefetch help and dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,13 +6,13 @@ export default async function HomePage() {
     <div className="flex flex-col items-center justify-center min-h-screen min-w-fit bg-gray-50">
       <h1 className="text-7xl font-bold mb-8 font-gau-pop-magic text-red-500 animate-bounceIn">Keiko!</h1>
       <div className="flex flex-col items-center space-y-4 w-50 max-w-xs">
-        <Link href="/dashboard?view=courses" className="w-full">
+        <Link prefetch href="/dashboard?view=courses" className="w-full">
           <Button className="w-full bg-white text-red-500 hover:bg-red-500 hover:text-white border border-red-500 font-gau-pop-magic">
             ⟡ Start
           </Button>
         </Link>
 
-        <Link href="/help" className="w-full">
+        <Link prefetch href="/help" className="w-full">
           <Button className="w-full bg-red-500 text-white hover:bg-white hover:text-red-500 border border-red-500 font-gau-pop-magic">
             ✦ How it works
           </Button>
